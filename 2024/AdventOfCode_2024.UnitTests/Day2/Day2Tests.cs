04f85b6f5b37486809366a6600dd012fd286e90e
didier.cauvin <didier.cauvin@mediane.tm.fr>
2024-12-03 08:39:46 +0100

    marche pas

--- 2024/AdventOfCode_2024.UnitTests/Day2/Day2Tests.cs
+++ 2024/AdventOfCode_2024.UnitTests/Day2/Day2Tests.cs
@@ -1,4 +1,5 @@
-﻿using static AdventOfCode_2024.UnitTests.Day2.InputInfo;
+﻿using System.Runtime.CompilerServices;
+using static AdventOfCode_2024.UnitTests.Day2.InputInfo;
 
 namespace AdventOfCode_2024.UnitTests.Day2;
 
@@ -121,48 +122,114 @@ public class Day2Tests
 
         Assert.AreEqual(479, nbSafe);
     }
+
+    [TestMethod]
+    public void Part2_HandleTwoErrorsFromInput()
+    {
+        int nbSafe = Reporter.HowManySafe("69 72 73 76 77 74 75 75", true);
+
+        Assert.AreEqual(0, nbSafe);
+    }
+
+    [DataTestMethod]
+    public void Part2_SafeByRemovingSecondLevel()
+    {
+        var input = @"7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9";
+
+        int nbSafe = Reporter.HowManySafe(input, true);
+
+        Assert.AreEqual(4, nbSafe);
+    }
+
+    [TestMethod]
+    public void Part2_FindHowManySafeFromInputWithDampener()
+    {
+        int nbSafe = Reporter.HowManySafe(Input, true);
+
+        Assert.AreEqual(567, nbSafe);
+    }
 }
 
 public static class Reporter
 {
-    public static int HowManySafe(string input)
+    public static int HowManySafe(string input, bool applyDampener = false)
     {
         var levels = input.Split("\n").Select(l => l.Split(" ").Select(s => int.Parse(s)).ToArray()).ToArray();
 
-        return HowManySafe(levels);
+        return HowManySafe(levels, applyDampener);
     }
 
-    private static int HowManySafe(int[][] levels)
+    private static int HowManySafe(int[][] levels, bool applyDampener)
     {
         var nbSafe = 0;
 
         foreach (var line in levels)
         {
-            var lineSafe = true;
-
-            var asc = line.OrderBy(x => x).ToArray();
-            var desc = asc.OrderByDescending(x => x).ToArray();
-
-            for (var i = 0; i < line.Length; i++)
-            {
-                var nextIndex = i + 1;
-
-                if (IsLineUnsafe(line, asc, desc, i, nextIndex))
-                {
-                    lineSafe = false;
-                    break;
-                }
-            }
-
-            if (lineSafe) nbSafe++;
+            if (IsLineSafe(line, applyDampener)) nbSafe++;
         }
 
         return nbSafe;
     }
 
-    private static bool IsLineUnsafe(int[] line, int[] asc, int[] desc, int i, int nextIndex)
+    private static bool IsLineSafe(int[] line, bool applyDampener)
     {
-        return !line.SequenceEqual(asc) && !line.SequenceEqual(desc) ||
+        var lineSafe = true;
+
+        for (var i = 0; i < line.Length; i++)
+        {
+            var asc = line.OrderBy(x => x).ToArray();
+            var desc = asc.OrderByDescending(x => x).ToArray();
+
+            if ((!line.SequenceEqual(asc) && !line.SequenceEqual(desc)) || IsLineUnsafe(line, i))
+            {
+                lineSafe = false;
+
+                if (applyDampener)
+                {
+                    lineSafe = !IsLineUnsafeDampered(line.Skip(1).ToArray(), line, 0);
+                }
+            }
+        }
+
+        return lineSafe;
+    }
+
+    private static bool IsLineUnsafeDampered(int[] line, int[] refLines, int indexToSkip)
+    {
+        var isLineUnsafe = true;
+
+        var asc = line.OrderBy(x => x).ToArray();
+        var desc = asc.OrderByDescending(x => x).ToArray();
+
+        if (indexToSkip < line.Length - 1)
+        {
+            for(int j = 0; j < line.Length; j++)
+            {
+                isLineUnsafe = (!line.SequenceEqual(asc) && !line.SequenceEqual(desc)) || IsLineUnsafe(line, j);
+
+                if (isLineUnsafe)
+                {
+                    indexToSkip++;
+                    return IsLineUnsafeDampered(refLines.Where((_, index) => index != indexToSkip).ToArray(), refLines, indexToSkip);
+                }
+            }
+
+            isLineUnsafe = false;
+        }
+
+        return isLineUnsafe;
+    }
+
+    private static bool IsLineUnsafe(int[] line, int i)
+    {
+        var nextIndex = i + 1;
+
+        return 
             (nextIndex < line.Length && (line[i] == line[nextIndex] ||
             Math.Abs(line[i] - line[nextIndex]) < 1 || Math.Abs(line[i] - line[nextIndex]) > 3));
     }
